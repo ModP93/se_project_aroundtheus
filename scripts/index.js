e943228578d63a374b7dc8ff135d2b3fb2ea51f3
ModP93 <moddymarch@yahoo.com>
2024-11-08 00:16:19 -0500

    added js card links

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards=[
+    {
+        title:'Dripping Springs'
+        link:'https://unsplash.com/photos/trees-under-blue-sky-KkbOHHJP5CQ'
+    }
+    {
+        title:'Fairfield'
+        link:'https://unsplash.com/photos/milky-way-photography-above-concrete-building-b3PuuRU8IPc'
+    }
+    {
+        title:'Mammoth Lakes'
+        link:'https://unsplash.com/photos/silhouette-of-mountains-under-milky-way-galaxy--wEFdRCG4IU'
+    }
+    {
+        title:'Olympic Peninsula'
+        link:'https://unsplash.com/photos/silhouette-photo-of-mountain-during-nighttime-syhd5N6nceM'
+    }
+    {
+        title:'Solheimasandur Plane Wreck'
+        link:'https://unsplash.com/photos/plane-on-brown-soil--HPhkZcJQNk'
+    }
+    {
+        title:'Yosemite Valley'
+        link:'https://unsplash.com/photos/bottom-view-shot-of-trees-under-starry-sky-Rr9zn33OMbk'
+    }
+]
