c10881379efcb8ea16945c68a0f60a1611142121
ModP93 <moddymarch@yahoo.com>
2024-11-08 08:33:06 -0500

    js image links fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const initialCards=[
     {
         name:'Dripping Springs',
-        link:'https://unsplash.com/photos/trees-under-blue-sky-KkbOHHJP5CQ'
+        link:'https://images.unsplash.com/photo-1476159093398-35ea18d2cdee?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },    
     {
         name:'Fairfield',
-        link:'https://unsplash.com/photos/milky-way-photography-above-concrete-building-b3PuuRU8IPc'
+        link:'https://images.unsplash.com/photo-1531177071211-ed1b7991958b?q=80&w=3458&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         name:'Mammoth Lakes',
-        link:'https://unsplash.com/photos/silhouette-of-mountains-under-milky-way-galaxy--wEFdRCG4IU'
+        link:'https://images.unsplash.com/photo-1498611291069-aa296192f1e4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         name:'Olympic Peninsula',
-        link:'https://unsplash.com/photos/silhouette-photo-of-mountain-during-nighttime-syhd5N6nceM'
+        link:'https://images.unsplash.com/photo-1528827352060-35c8861f19f4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzV8fG5pZ2h0JTIwc2t5fGVufDB8fDB8fHww'
     },
     {
         name:'Solheimasandur Plane Wreck',
-        link:'https://unsplash.com/photos/plane-on-brown-soil--HPhkZcJQNk'
+        link:'https://images.unsplash.com/photo-1472419551702-dc4862cad8f9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         name:'Yosemite Valley',
-        link:'https://unsplash.com/photos/bottom-view-shot-of-trees-under-starry-sky-Rr9zn33OMbk'
+        link:'https://images.unsplash.com/photo-1460131548611-36aac9b6a901?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     }
 ]
