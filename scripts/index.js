494afaf39dcd87e63f5e389d82d046e0d58e889e
ModP93 <moddymarch@yahoo.com>
2024-11-08 08:41:01 -0500

    js images again

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const initialCards=[
     {
         name:'Dripping Springs',
-        link:'https://images.unsplash.com/photo-1476159093398-35ea18d2cdee?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg'
+        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_dripping-springs.jpg'
     },    
     {
         name:'Fairfield',
-        link:'https://images.unsplash.com/photo-1531177071211-ed1b7991958b?q=80&w=3458&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg'
+        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_fairfield-star-house.jpg'
     },
     {
         name:'Mammoth Lakes',
-        link:'https://images.unsplash.com/photo-1498611291069-aa296192f1e4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg'
+        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_mammoth-lakes.jpg'
     },
     {
         name:'Olympic Peninsula',
-        link:'https://images.unsplash.com/photo-1528827352060-35c8861f19f4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzV8fG5pZ2h0JTIwc2t5fGVufDB8fDB8fHww.jpg'
+        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_olympic-peninsula-washington.jpg'
     },
     {
         name:'Solheimasandur Plane Wreck',
-        link:'https://images.unsplash.com/photo-1472419551702-dc4862cad8f9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg'
+        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_solheimasandur-plane-wreck-iceland.jpg'
     },
     {
         name:'Yosemite Valley',
-        link:'https://images.unsplash.com/photo-1460131548611-36aac9b6a901?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg'
+        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_yosemite-valley.jpg'
     }
 ]
