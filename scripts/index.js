884f115deed1872c3fc71a90e3873f05b1a92e97
ModP93 <moddymarch@yahoo.com>
2024-11-08 00:43:52 -0500

    js change object names

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const initialCards=[
     {
-        title:'Dripping Springs',
+        name:'Dripping Springs',
         link:'https://unsplash.com/photos/trees-under-blue-sky-KkbOHHJP5CQ'
     },    
     {
-        title:'Fairfield',
+        name:'Fairfield',
         link:'https://unsplash.com/photos/milky-way-photography-above-concrete-building-b3PuuRU8IPc'
     },
     {
-        title:'Mammoth Lakes',
+        name:'Mammoth Lakes',
         link:'https://unsplash.com/photos/silhouette-of-mountains-under-milky-way-galaxy--wEFdRCG4IU'
     },
     {
-        title:'Olympic Peninsula',
+        name:'Olympic Peninsula',
         link:'https://unsplash.com/photos/silhouette-photo-of-mountain-during-nighttime-syhd5N6nceM'
     },
     {
-        title:'Solheimasandur Plane Wreck',
+        name:'Solheimasandur Plane Wreck',
         link:'https://unsplash.com/photos/plane-on-brown-soil--HPhkZcJQNk'
     },
     {
-        title:'Yosemite Valley',
+        name:'Yosemite Valley',
         link:'https://unsplash.com/photos/bottom-view-shot-of-trees-under-starry-sky-Rr9zn33OMbk'
     }
 ]
