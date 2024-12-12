d18913027c1f77c0808e464325433435ae486d0d
ModP93 <moddymarch@yahoo.com>
2024-12-12 11:24:11 -0500

    java cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,48 @@
 const initialCards=[
     {
-        name:'Dripping Springs',
-        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_dripping-springs.jpg'
+        name:'Yosemite Valley',
+        link:'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg'
     },    
     {
-        name:'Fairfield',
-        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_fairfield-star-house.jpg'
+        name:'Lake Louise',
+        link:'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg'
     },
     {
-        name:'Mammoth Lakes',
-        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_mammoth-lakes.jpg'
+        name:'Bald Mountains',
+        link:'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg'
     },
     {
-        name:'Olympic Peninsula',
-        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_olympic-peninsula-washington.jpg'
+        name:'Latemar',
+        link:'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg'
+
     },
     {
-        name:'Solheimasandur Plane Wreck',
-        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_solheimasandur-plane-wreck-iceland.jpg'
+        name:'Vanoise National Park',
+        link:'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg'
     },
     {
-        name:'Yosemite Valley',
-        link:'\Users\amond\Projects\se_project_aroundtheus\images\jpeg-optimizer_yosemite-valley.jpg'
+        name:'Lago di Braies',
+        link:'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg'
     }
-]
+    ]
 
 const profile=document.querySelectorAll('.profile');
 const editButton=document.querySelector('.profile__edit-button');
 const profileEditModal=document.querySelector('.modal');
 const modalContainer=document.querySelector('.modal__container');
 const editCloseButton=document.querySelector('.modal__close-button');
+const profileName=document.querySelector('.profile__name');
+const profileDescription=document.querySelector('.profile__description');
+const profileNameInput=document.querySelector('#profile-name-input');
+const profileDescriptionInput=document.querySelector('#profile-description-input');
+const profileEditModalForm=profileEditModal.querySelector('.modal__form');
+const cardGallaryElement=document.querySelector('.cards__gallery');
+const cardTemplate=document.querySelector('#card-template').content.firstElementChild;
 
-function editProfile() {
+ function editProfile() {
+    profileNameInput.value=profileName.textContent;
+    profileDescriptionInput.value=profileDescription.textContent;
+
     profileEditModal.classList.add('modal__open');
 }
 
@@ -39,5 +50,26 @@ function closeEditProfile() {
     profileEditModal.classList.remove('modal__open');
 }
 
+function getCardElement(data) {
+const cardElement=cardTemplate.cloneNode(true);
+const cardImageElement=cardElement.querySelector('.card__image');
+const cardTitleElement=cardElement.querySelector('.card__title');
+cardImageElement.src=data.link
+cardImageElement.alt=data.name
+cardTitleElement.textContent=data.name
+return cardElement;
+}
+
 editButton.addEventListener('click', editProfile);
 editCloseButton.addEventListener('click', closeEditProfile);
+profileEditModalForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    profileName.textContent=profileNameInput.value;
+    profileDescription.textContent=profileDescriptionInput.value;
+    closeEditProfile();
+})
+
+initialCards.forEach((data) => {
+    const cardElement=getCardElement(data);
+    cardGallaryElement.prepend(cardElement);
+})
